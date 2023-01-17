ff859f40c15494f3db015037ea940083f08a9d15
MattASmith18 <matt.180798@gmail.com>
2023-01-17 13:56:21 +0000

    start json creation

--- generateEmployee.js
+++ generateEmployee.js
@@ -1,10 +1,30 @@
-var arr = new Array(10).fill().map(function() {
-  return {
-      first: chance["first"](),
-      last: chance["last"](),
-      email: chance["email"](),
-      age: change[age](),
-  };
-});
+import { faker } from '@faker-js/faker';
+import fs from 'fs'
 
-var json = JSON.stringify({ users: arr }, null, 2);
+function generateUsers() {
+
+  let users = []
+
+  for (let id=1; id <= 40; id++) {
+
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let email = faker.internet.email();
+    let personAge = faker.date.birthdate({min: 18, max: 67, mode: 'age'});
+
+    users.push({
+        "id": id,
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email,
+        "age ": personAge
+    });
+  }
+
+  return { "data": users }
+}
+
+let dataObj = generateUsers();
+
+fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
+
