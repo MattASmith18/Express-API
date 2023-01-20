3bc82cc2322f07f78e1c24502a9571db6afa9a65
MattASmith18 <matt.180798@gmail.com>
2023-01-20 13:00:38 +0000

    updated changes from thoms edit

--- generateEmployee.js
+++ generateEmployee.js
@@ -1,38 +1,29 @@
 //import {faker} from '@faker-js/faker';
-const faker = require('@faker-js/faker');
-//import fs from 'fs'
-const fs = require('fs');
-
+const { faker } = require('@faker-js/faker');
 
 function generateUsers() {
 
   let users = []
 
-  for (let id=1; id <= 50; id++) {
+  for (let id = 1; id <= 50; id++) {
 
     let firstName = faker.name.firstName();
     let lastName = faker.name.lastName();
     let email = faker.internet.email();
-    let personAge = faker.datatype.number({'min': 18, 'max': 67 });
+    let personAge = faker.datatype.number({ 'min': 18, 'max': 67 });
 
     users.push({
-        "id": id,
-        "first_name": firstName,
-        "last_name": lastName,
-        "email": email,
-        "age ": personAge
+      "id": id,
+      "first_name": firstName,
+      "last_name": lastName,
+      "email": email,
+      "age ": personAge
     });
   }
 
   return { "Employees": users }
 }
 
-
-// module.exports = function create_Json(){
-//   let dataObj = generateUsers();
-//   fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
-// }
-
-module.exports = {generateUsers};
+module.exports = { generateUsers };
 
 
